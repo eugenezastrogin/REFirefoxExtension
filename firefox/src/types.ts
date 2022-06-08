c43a587f9feb1cea8102d426aa7e19fc341b7f9d
Eugene <aethelz@users.noreply.github.com>
2022-06-08 23:27:45 +0200

    Add lost types

---
+++ firefox/src/types.ts
@@ -0,0 +1,15 @@
+export type REData = number[];
+export type CriticalPower = number;
+export type FormPower = number;
+export type StoredSettings = {
+  weight: number;
+  cp: number;
+  strideLengthToggle: boolean;
+  cpToggle: boolean;
+  fprToggle: boolean;
+  wpkgToggle: boolean;
+};
+export type Seconds = number;
+export type Meters = number;
+export type Watts = number;
+export type Cadence = number;
